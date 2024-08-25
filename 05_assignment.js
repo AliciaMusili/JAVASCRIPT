d9c45ff4783ff156210c3f975cc18e044f92c584
Alicia Musili <aliciamusili2410@gmail.com>
2024-08-25 22:37:56 +0300

    functions assignment

---
+++ 05_assignment.js
@@ -0,0 +1,25 @@
+// TASK - using an if else if statement , build a grading system with the following conditions
+//condtions >80 A, >65 B , >50 C, >40 D  , <41 E
+// <0 or >100 is an invalid mark (not be graded)
+
+function gradeSystem(mark) {
+    if (mark < 0 || mark > 100) {
+        return "Invalid mark (not be graded)";
+    } else if (mark > 80) {
+        return "A";
+    } else if (mark > 65) {
+        return "B";
+    } else if (mark > 50) {
+        return "C";
+    } else if (mark > 40) {
+        return "D";
+    } else {
+        return "E";
+    }
+}
+
+// Example usage
+const marks = [85, 70, 55, 45, 30, -5, 105];
+marks.forEach(mark => {
+    console.log(`Mark: ${mark}, Grade: ${gradeSystem(mark)}`);
+});
