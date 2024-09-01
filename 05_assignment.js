0bb849f81d4440a0fb7f50d8ff1ce13c7e09e413
Alicia Musili <aliciamusili2410@gmail.com>
2024-09-01 20:34:54 +0300

    QUESTION ONE

--- 05_assignment.js
+++ 05_assignment.js
@@ -23,3 +23,29 @@ const marks = [85, 70, 55, 45, 30, -5, 105];
 marks.forEach(mark => {
     console.log(`Mark: ${mark}, Grade: ${gradeSystem(mark)}`);
 });
+
+
+//redo it with switch statement
+
+function gradeSystem(mark) {
+    switch (true) {
+        case (mark < 0 || mark > 100):
+            return "Invalid mark (not be graded)";
+        case (mark > 80):
+            return "A";
+        case (mark > 65):
+            return "B";
+        case (mark > 50):
+            return "C";
+        case (mark > 40):
+            return "D";
+        default:
+            return "E";
+    }
+}
+
+// Example usage
+const marks = [85, 70, 55, 45, 30, -5, 105];
+[85, 70, 55, 45, 30, -5, 105].forEach(mark => {
+    console.log(`Mark: ${mark}, Grade: ${gradeSystem(mark)}`);
+});
